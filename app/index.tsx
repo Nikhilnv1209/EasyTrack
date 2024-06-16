9cf72a73a02f5f74e8ae7706521c1a30b54c7e9b
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-06-16 11:17:58 +0530

    Feat(project-structure): add assets, constants, and font setup
    - Add 'assets' folder with required graphic and media files for the project.
    
    - Create 'constants' folder for managing global constants across the application.
    
    - Setup fonts in the '_layout.tsx' file to enhance the global UI layout.

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className='h-full flex items-center justify-center'>
-      <Text>This is the index page using expo router!</Text>
+      <Text className='text-xl font-pmedium px-3 py-3 border border-black rounded-lg mx-12'>This is the index page using expo router!</Text>
       <StatusBar style="auto" />
     </View>
   );
