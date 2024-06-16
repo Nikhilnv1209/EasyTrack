ee7956cf98b901da760527aa6c731aa9bf10bfd0
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-06-16 11:31:52 +0530

    Feat(structure): establish initial folder structure for the app
    - Create basic directory layout to organize project files and components.
    
    - Include folders for components, utilities, assets, and services as needed.
    
    - Set up foundational project files for smooth development workflow.

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View className='h-full flex items-center justify-center'>
-      <Text className='text-xl font-pmedium px-3 py-3 border border-black rounded-lg mx-12'>This is the index page using expo router!</Text>
+      <Text className='text-base font-pmedium'>This is the index page using expo router!</Text>
       <StatusBar style="auto" />
+      <Link href={"/home"} className='text-blue-400 text-lg pt-4 underline'>Go to home</Link>
     </View>
   );
 }
