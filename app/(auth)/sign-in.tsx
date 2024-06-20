0b02d7578b7527c61fe0da255fbd629a85399d1e
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-06-20 21:08:06 +0530

    Feat(auth-integration): utilize Appwrite functions in sign-in and sign-up forms
    - Integrate Appwrite backend functions in the submit handlers of the sign-in and sign-up forms.
    
    - Enable user account creation and session management through Appwrite during authentication.
    
    - Enhance form validation and error handling to ensure robust user interactions.

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import Button from '../../components/Button'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { Signin } from '../../lib/appwrite'
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -14,8 +15,26 @@ const SignIn = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submitForm = () => {}
+  const submitForm = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert('Error', 'Please fill all fields');
+    }
+    setIsSubmitting(true);
+    try {
+      await Signin(form.email, form.password);
 
+      router.replace('/home');
+    } catch (error: unknown) {
+      console.error(error);
+      if (error instanceof Error) {
+        Alert.alert('Error', error.message);
+      } else {
+        Alert.alert('Error', 'An Unknown error occurred');
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
 
   return (
     <SafeAreaView className='bg-primary h-full'>
