e94efc0a3016e3ae6e338e79a29ccabf64810fde
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-06-23 23:16:07 +0530

    Chore(home-cleanup): format Home component with Prettier and add comments
    - Apply code formatting standards using Prettier to enhance code readability.
    
    - Add descriptive comments to improve code maintainability and understanding.

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,77 +1,87 @@
-import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../../constants'
-import SearchInput from '../../components/SearchInput'
-import Trending from '../../components/Trending'
-import EmptyState from '../../components/EmptyState'
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  RefreshControl,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
 
 export interface Post {
-  id: number
+  id: number;
 }
 
 const Home = () => {
   const [refreshing, setRefreshing] = useState(false);
-  
-  const onRefresh = async() => {
+
+  const onRefresh = async () => {
     setRefreshing(true);
-    
+
     // refresh data
 
     setRefreshing(false);
-  }
+  };
   return (
-    <SafeAreaView className='bg-primary h-full'>
+    <SafeAreaView className="bg-primary h-full">
       <FlatList<Post>
         data={[{ id: 1 }, { id: 2 }]}
-        // data={[]}
         keyExtractor={(item) => item.id.toString()}
-        className='px-4'
+        className="px-4"
         renderItem={({ item }) => (
-          <Text className='text-xl text-white'>{item.id}</Text>
+          <Text className="text-xl text-white">{item.id}</Text>
         )}
         ListHeaderComponent={() => {
           return (
-            <View className='my-4 space-y-6'>
-              <View className='flex-row items-start justify-between'>
-                <View className='space-y-1'>
-                  <Text className='text-base font-pmedium text-gray-100'>
+            <View className="my-4 space-y-6">
+              <View className="flex-row items-start justify-between">
+                <View className="space-y-1">
+                  <Text className="text-base font-pmedium text-gray-100">
                     Welcome Back
                   </Text>
-                  <Text className='text-3xl font-psemibold text-white'>
+                  <Text className="text-3xl font-psemibold text-white">
                     Nikhilnv
                   </Text>
                 </View>
-
-                <View className='mt-1.5'>
-                  <Image source={images.logoSmall} className='w-10 h-7' resizeMode='contain' />
+                <View className="mt-1.5">
+                  <Image
+                    source={images.logoSmall}
+                    className="w-10 h-7"
+                    resizeMode="contain"
+                  />
                 </View>
               </View>
 
+              {/* Search Box to search for a video */}
               <SearchInput />
 
+              {/* Trending videos component */}
               <View>
-                <Text className='text-lg font-pregular text-gray-100'>
+                <Text className="text-lg font-pregular text-gray-100">
                   Trending videos
                 </Text>
+                <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
               </View>
 
-              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
             </View>
-          )
+          );
         }}
-
         ListEmptyComponent={() => (
           <EmptyState
-            title='No videos found'
-            subtitle='Be the first to upload a video!'
+            title="No videos found"
+            subtitle="Be the first to upload a video!"
           />
         )}
-
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
-    </SafeAreaView >
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
