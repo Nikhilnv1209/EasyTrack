cb58717c47f8525f7754a98ac0588557d11fba6c
nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-06-30 22:41:01 +0530

    Feat(trendings-update): animate Trendings component with react-native-animatable
    Chore(dependencies): update package.json and package-lock.json
    
    - Refactor Trendings component to display trending posts in an animated state using react-native-animatable.
    
    - Implement animations for non-playing post logic.
    
    - Add react-native-animatable to dependencies, updating package files accordingly.

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,19 +1,87 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import { Text, FlatList, TouchableOpacity, ImageBackground, Image, ViewToken } from 'react-native'
+import React, { useState } from 'react'
 import { Post } from '../lib/useAppwrite'
+import * as Animatable from 'react-native-animatable'
+import { icons } from '../constants'
 
 interface TrendingProps {
     posts: Array<Post>
 }
 
-const Trending = ({ posts }: TrendingProps) => {
+const zoomIn = {
+    0: {
+        scale: 0.9,
+    },
+    1: {
+        scale: 1,
+    },
+};
+
+const zoomOut = {
+    0: {
+        scale: 1,
+    },
+    1: {
+        scale: 0.9,
+    },
+};
+
+const TrendingItem = ({ activeItem, item }: { activeItem: string, item: Post }) => {
+    const [play, setPlay] = useState(false);
     return (
-        <FlatList
+        <Animatable.View
+            className='mr-2'
+            // @ts-ignore
+            animation={activeItem === item.$id ? zoomIn : zoomOut}
+            duration={500}
+        >
+            {play ? (
+                <Text className='text-white'>
+                    playing
+                </Text>
+            ) : (
+                <TouchableOpacity className='relative justify-center items-center' 
+                    activeOpacity={0.7}
+                    onPress={() => setPlay(true)}
+                    >
+                    <ImageBackground
+                        source={{ uri: item.thumbnail }}
+                        className='w-40 h-60 rounded-[30px] my-5 overflow-hidden shadow-lg shadow-black/40'
+                        resizeMode='cover'
+                    />
+                    <Image
+                        source={icons.play}
+                        className='w-10 h-10 absolute opacity-90'
+                        resizeMode='contain'
+                    />
+                </TouchableOpacity>
+            )}
+        </Animatable.View>
+    )
+}
+
+const Trending = ({ posts }: TrendingProps) => {
+    const [activeItem, setActiveItem] = useState<string>("");
+
+    const onViewableItemsChanged = ({ viewableItems }: { viewableItems: ViewToken<Post>[] }) => {
+        setActiveItem(viewableItems[0].key);
+    }
+    
+    return (
+        <FlatList<Post>
             data={posts}
             keyExtractor={(item) => item.$id.toString()}
             renderItem={({ item }) => (
-                <Text className='text-xl text-white'>{item.id}</Text>
+                <TrendingItem
+                    activeItem={activeItem}
+                    item={item}
+                />
             )}
+            onViewableItemsChanged={onViewableItemsChanged}
+            viewabilityConfig={{
+                itemVisiblePercentThreshold: 70
+            }}
+            contentOffset={{ x: 80, y: 0}}
             horizontal
         />
     )
